d46697f9b34a0c9f84e3b667e5b79de194ed70de
My Name <my-name@chromium.org>
2024-04-24 16:28:55 -0700

    Init source code import

---
+++ win/TXTView.h
@@ -0,0 +1,31 @@
+// View.h : interface of the CTXTView class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CTXTView : public CScrollWindowImpl<CTXTView>
+{
+public:
+	DECLARE_WND_CLASS(NULL)
+
+	BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		pMsg;
+		return FALSE;
+	}
+
+	void DoPaint(CDCHandle dc)
+	{
+		//TODO: Add your drawing code here
+	}
+
+	BEGIN_MSG_MAP(CTXTView)
+		CHAIN_MSG_MAP(CScrollWindowImpl<CTXTView>)
+	END_MSG_MAP()
+
+// Handler prototypes (uncomment arguments if needed):
+//	LRESULT MessageHandler(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+//	LRESULT CommandHandler(WORD /*wNotifyCode*/, WORD /*wID*/, HWND /*hWndCtl*/, BOOL& /*bHandled*/)
+//	LRESULT NotifyHandler(int /*idCtrl*/, LPNMHDR /*pnmh*/, BOOL& /*bHandled*/)
+};
---
+++ win/View.h
@@ -0,0 +1,28 @@
+// View.h : interface of the CView class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CView : public CWindowImpl<CView>
+{
+public:
+	DECLARE_WND_CLASS(NULL)
+
+		BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		pMsg;
+		return FALSE;
+	}
+
+	BEGIN_MSG_MAP(CView)
+		MESSAGE_HANDLER(WM_PAINT, OnPaint)
+	END_MSG_MAP()
+
+	LRESULT OnPaint(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		CPaintDC dc(m_hWnd);
+
+		return 0;
+	}
+};
